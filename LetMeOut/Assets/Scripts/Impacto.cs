00a287fa7b5536da959b7f000b5bd3b87e0236f0
slopmat <120409284+slopmat@users.noreply.github.com>
2023-02-21 15:53:39 +0100

    VersionPulsador

---
+++ LetMeOut/Assets/Scripts/Impacto.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Impacto : MonoBehaviour
+{
+    private bool Impacticado = false;
+    GameObject Bacteria;
+
+    void Start()
+    {
+        Bacteria = GameObject.FindWithTag("Bacteria");
+    }
+
+    private void Update()
+    {
+        if (Impacticado == true)
+        {
+            Bacteria.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Bacteria")
+        {
+            Impacticado = true;
+        }
+    }
+}

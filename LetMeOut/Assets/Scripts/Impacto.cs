9ec3c6d0633cb6fedfe2e4a166157bffca2d7e80
slopmat <120409284+slopmat@users.noreply.github.com>
2023-02-22 17:45:23 +0100

    CorreccionScript

--- LetMeOut/Assets/Scripts/Impacto.cs
+++ LetMeOut/Assets/Scripts/Impacto.cs
@@ -5,27 +5,24 @@ using UnityEngine;
 
 public class Impacto : MonoBehaviour
 {
+    private Animator anim;
     private bool Impacticado = false;
-    GameObject Bacteria;
+    private GameObject Bacteria;
+    private BoxCollider2D boxCollider2D;
 
     void Start()
     {
+        anim = GetComponent<Animator>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
         Bacteria = GameObject.FindWithTag("Bacteria");
-    }
-
-    private void Update()
-    {
-        if (Impacticado == true)
-        {
-            Bacteria.SetActive(false);
-        }
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Bullet")
         {
-            Impacticado = true;
+            anim.Play("ZombieDeath");
+            boxCollider2D.enabled = false;
         }
     }
 }
